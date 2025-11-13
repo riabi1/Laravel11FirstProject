652aa04c48488fd249e1a7fbb0e70b28df8ed6c0
riabi1 <riabimedali18@gmail.com>
2025-11-13 15:57:15 +0100

    MVC: Model, Controller and View with Foreach

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $allCategories = Category::all();
+        return view('home', ['categories' => $allCategories]);
+    }
+}
