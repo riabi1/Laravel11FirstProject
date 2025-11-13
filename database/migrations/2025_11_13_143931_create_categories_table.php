4c98321020e35d6999706e7672e6c6f325760195
riabi1 <riabimedali18@gmail.com>
2025-11-13 15:44:34 +0100

    DB Structure, Migrations and .env Config

---
+++ database/migrations/2025_11_13_143931_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
+
+// php artisan make:migration "create categories table"
+// php artisan migrate:rollback
+
+// Check Current DB Schema
+// php artisan db:show
+// php artisan db:table
